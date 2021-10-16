4a577e252089a7946196675d1e0a046123c073bd
Tychon <tisha.kruglyak@gmail.com>
2021-10-16 15:05:01 +0200

    Basic stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.scss';
 import { Component } from 'react';
+import axios from 'axios'
 
+import Searchbar from './components/Form'
+
+const baseUrl = 'https://pixabay.com/api/';
+const api_key = '23036221-d804a8a78d7b0866edf7d8fc3';
+
+let searchQuery = '';
+let searchPage = 1;
+let url = `${baseUrl}?key=${api_key}&q=${searchQuery}&page=${searchPage}&image_type=photo&orientation=horizontal&per_page=12`
+
+axios.get(url).then((data) => {console.log(data)}).catch((err) => {console.log(err)})
 
 class App extends Component {
 
   render() { 
-    
+    return (
+      <Searchbar />
+    )
   }
 }
 
